02481f848b4da19d34530fa0d62637d177e83442
Andreas Kalberer <aka@swissqprint.com>
2023-04-13 14:31:29 +0200

    Add kernel command line parameter to reboot after 30s on kernel panic.

--- include/configs/tqma57xx.h
+++ include/configs/tqma57xx.h
@@ -306,6 +306,7 @@
 		"rootfstype=${mmcrootfstype}\0" \
 		"isolcpus=domain,1\0" \
 		"nohz_full=1\0" \
+		"panic=30\0" \
 	"loadbootscript=setenv bootpart ${mmcdev}:1; load ${devtype} ${bootpart} ${loadaddr} boot.scr\0" \
 	"bootscript=echo Running bootscript from mmc${mmcdev} ...; " \
 		"source ${loadaddr}\0" \
